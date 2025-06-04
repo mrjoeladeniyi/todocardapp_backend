061974d448fd0c43db5028dfa238ec28b17a5079
mrjoeladeniyi <joeloadeniyi@gmail.com>
2025-06-04 03:20:01 +0100

    chore: adjust moder User import in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/users');
+const User = require('../models/Users');
 const jwt = require('jsonwebtoken');
 const auth = require('../middleware/auth');
 
